a0df90da4c482af0a1ea058f437c182976601a07
Tommaso Ornato <tommy.ornato@gmail.com>
2023-12-22 19:11:56 +0100

    Adding thread scraping

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { Client, Events, GatewayIntentBits, GuildTextBasedChannel } from "discord.js";
+import { ChannelType, Client, Events, GatewayIntentBits, GuildTextBasedChannel, TextChannel } from "discord.js";
 import { Collection, MongoClient } from 'mongodb';
 import type { Message as Message_t } from './types.js';
 
@@ -18,6 +18,7 @@ discord.login(token);
 
 discord.once(Events.ClientReady, async discord => {
     console.log(`The scraper is ready! Logged in as ${discord.user.tag}`);
+    console.time('Scrape');
 
     await dbClient.connect();
     const db = dbClient.db('basedcount_scrape').collection<Message_t>('messages');
@@ -25,14 +26,38 @@ discord.once(Events.ClientReady, async discord => {
     const guild = discord.guilds.cache.get(serverId);
     if (!guild) throw new Error(`Can't find guild with ID ${serverId}`);
 
-    const channels = [...guild.channels.cache.values()].filter(ch => ch.isTextBased() || ch.isThread()) as GuildTextBasedChannel[];
+    const channels = guild?.channels.cache.filter((c): c is TextChannel => c.type === ChannelType.GuildText);
 
-    for (const channel of channels) {
-        console.log(`Now scraping: #${channel.name}`);
-        const found = await saveMessagesBefore(db, channel, '9223372036854775807', 0);
-        console.log(`${found} messages\n`);
+    for (const [_, channel] of channels) {
+        let messages = await saveMessages(db, channel, channel.name);
+
+        //Fetch open and closed threads, if any
+        const [closed, open] = await Promise.all([channel.threads.fetchArchived(), channel.threads.fetchActive()]);
+
+        for (const [_, thread] of closed.threads) {
+            messages += await saveMessages(db, thread, thread.name);
+        }
+
+        for (const [_, thread] of open.threads) {
+            messages += await saveMessages(db, thread, thread.name);
+        }
+
+        console.log(`Total: ${messages}\n`);
     }
+
+    console.timeEnd('Scrape');
+    process.exit();
 });
+
+async function saveMessages(db: Collection<Message_t>, channel: GuildTextBasedChannel, name: string) {
+    console.log(`Now scraping: "${name}"`);
+
+    const res = await saveMessagesBefore(db, channel, '9223372036854775807', 0);
+
+    console.log(`Found ${res} messages\n`);
+
+    return res;
+}
 
 async function saveMessagesBefore(db: Collection<Message_t>, channel: GuildTextBasedChannel, lastID: string, size: number): Promise<number> {
     const messageMap = await channel.messages.fetch({ limit: 100, before: lastID });    //The 100 messages limit comes from the Discord API
@@ -49,7 +74,7 @@ async function saveMessagesBefore(db: Collection<Message_t>, channel: GuildTextB
         timestamp: new Date(message.createdTimestamp),
     }));
 
-    await db.bulkWrite(messageArr.map(message => ({ insertOne: { document: message } })));
+    // await db.bulkWrite(messageArr.map(message => ({ insertOne: { document: message } })));
     console.log(`Scraped until ${messageArr[0].timestamp.toLocaleString()}`);
 
     return saveMessagesBefore(db, channel, messageArr[messageArr.length - 1].message_id, messageArr.length + size);
